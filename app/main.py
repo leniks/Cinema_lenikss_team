05f2fa17f68b82e7012531993d5e16383fb29bd6
lenikss <anciborko04@mail.ru>
2024-11-20 19:28:02 +0300

    starting_endpoints

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from utils import json_to_dict_list
+import os
+from typing import Optional
+
+app = FastAPI()
+# Получаем путь к JSON
+path_to_json = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'films.json')
+
+
+@app.get("/")
+def home_page():
+    return {"message": "bebra bebra bebra"}
+
+
+@app.get("/films")
+def get_all_films():
+    return json_to_dict_list(path_to_json)
+
+
+@app.get("/films/{film_id}")
+def get_film_by_id(film_id: int):
+    films = json_to_dict_list(path_to_json)
+    return_list = []
+
+    for film in films:
+        if film["id"] == film_id:
+            return_list.append(film)
+
+    return return_list
