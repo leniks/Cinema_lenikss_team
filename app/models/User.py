f47e2d6945e3e1ca69bdc997d52114a981686b61
leoan <anciborko04@mail.ru>
2024-12-26 17:12:06 +0300

    auth

--- app/models/User.py
+++ app/models/User.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Integer, String, Text, Date, TIMESTAMP, ForeignKey, UniqueConstraint, Table, func
+from sqlalchemy import Integer, String, Text, Date, TIMESTAMP, ForeignKey, UniqueConstraint, Table, func, text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -11,6 +11,9 @@ class User(Base):
     username: Mapped[str_uniq]
     email: Mapped[str_uniq]
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+
+    is_user: Mapped[bool] = mapped_column(default=True, server_default=text('true'), nullable=False)
+    is_admin: Mapped[bool] = mapped_column(default=False, server_default=text('false'), nullable=False)
 
     reviews: Mapped[list["Review"]] = relationship("Review", back_populates="user")
     favorites: Mapped[list["Favorite"]] = relationship("Favorite", back_populates="user")
