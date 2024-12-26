a661dff0448d3e25d0be0a36a771ec453a3b1b54
lenikss <anciborko04@mail.ru>
2024-12-26 23:31:03 +0300

    auth

--- app/models/user_watchlist.py
+++ app/models/user_watchlist.py
@@ -5,5 +5,5 @@ movie_genres = Table(
     'user_watchlist',
     Base.metadata,
     Column('user_id', Integer, ForeignKey('users.id'), primary_key=True),
-    Column('movie_id', Integer, ForeignKey('movie.id'), primary_key=True)
+    Column('movie_id', Integer, ForeignKey('movies.id'), primary_key=True)
 )
