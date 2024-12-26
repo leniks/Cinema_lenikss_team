83a9ebd5573d9709fa3ebc15746b4a1523b7073d
kr1st1na0 <kristina.bilckova@yandex.ru>
2024-12-27 03:11:22 +0800

    user_favorites&watchlist

--- app/models.py
+++ app/models.py
@@ -9,21 +9,9 @@ class Genre(BaseModel):
     genre_id: int
     name: str = Field(..., min_length=1, max_length=50, description="Название жанра от 1 до 50 символов")
 
-
-class Review(BaseModel):
-    review_id: int
-    user_id: int
-    film_id: int
-    rating: int = Field(..., ge=1, le=10, description="Оценка фильма от 1 до 10")
-    comment: Optional[str] = Field(default=None, max_length=500, description="Текст рецензии, не более 500 символов")
-    created_at: datetime = Field(default_factory=datetime.utcnow)
-    updated_at: datetime = Field(default_factory=datetime.utcnow)
-
-
 class Favorite(BaseModel):
     user_id: int
     film_id: int
-
 
 class Watchlist(BaseModel):
     user_id: int
--- app/services/users_service.py
+++ app/services/users_service.py
@@ -5,7 +5,6 @@ from sqlalchemy.exc import SQLAlchemyError
 from app.models.Watchlist import Watchlist
 from app.models.Favorite import Favorite
 from app.models.User import User
-from app.models.Review import Review
 from app.models.Genre import Genre
 from app.models.Movie import Movie
 
